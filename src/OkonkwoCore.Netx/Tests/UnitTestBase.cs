8ad26ce201827a64162429a729bf65b803ea13f1
Chris Okonkwo <Chris.Okonkwo@argodata.com>
2020-04-03 15:26:53 -0500

    add UnitTestBase

---
+++ src/OkonkwoCore.Netx/Tests/UnitTestBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OkonkwoCore.Netx.Tests
+{
+    public abstract class UnitTestBase
+    {
+        [TestInitialize]
+        public virtual void Initialize()
+        {
+            // add common test tasks here
+        }
+    }
+}
