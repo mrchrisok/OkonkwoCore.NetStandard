6b203e395afc23f91eb53a032bcf909ab4861a59
Chris Okonkwo <Chris.Okonkwo@argodata.com>
2020-04-03 20:52:50 -0500

    add SimpleMapper

---
+++ src/OkonkwoCore.Netx/Utilities/SimpleMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace OkonkwoCore.Netx.Utilities
+{
+    public static class SimpleMapper
+    {
+        public static void PropertyMap<T, U>(T source, U destination)
+           where T : class, new()
+           where U : class, new()
+        {
+            var sourceProperties = source.GetType().GetProperties().ToList();
+            var destinationProperties = destination.GetType().GetProperties().ToList();
+
+            foreach (var sourceProperty in sourceProperties)
+            {
+                var destinationProperty = destinationProperties.Find(item => item.Name == sourceProperty.Name);
+
+                if (destinationProperty != null)
+                {
+                    try
+                    {
+                        destinationProperty.SetValue(destination, sourceProperty.GetValue(source, null), null);
+                    }
+                    catch (ArgumentException)
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
