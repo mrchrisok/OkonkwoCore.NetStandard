6749e5396d3d66b4630cc3eac8ca57e79714d21d
Chris Okonkwo <Chris.Okonkwo@argodata.com>
2020-04-23 12:58:21 -0500

    add CreatedBy and ModifiedBy

--- src/OkonkwoCore.Netx/Contracts/IChangesTrackedEntity.cs
+++ src/OkonkwoCore.Netx/Contracts/IChangesTrackedEntity.cs
@@ -5,6 +5,8 @@ namespace OkonkwoCore.Netx.Contracts
     public interface IChangesTrackedEntity
     {
         DateTime? DateCreated { get; set; }
+        string CreatedBy { get; set; }
         DateTime? DateModified { get; set; }
+        string ModifiedBy { get; set; }
     }
 }
